fe39c08572ceaa43a44a529b0424c8c39bd941e9
pparonson <preston.aronson@gmail.com>
2018-06-12 20:53:01 -0600

    add left unitSection

--- src/model.js
+++ src/model.js
@@ -1,5 +1,23 @@
 const initModel = {
-  description: "initModel"
+  leftValue: 0
+  , leftUnit: "Celcius"
+  , rightValue: 32
+  , rightUnit: "Fahrenheit"
+  , isLeftSource: true
 };
+
+// helpers
+function celciusToFahrenheit(_model) {
+  const {temperature} = _model;
+  // (T(°C) = (T(°F) - 32) / 1.8
+  return ( (temperature - 32) / 1.8 );
+}
+
+function fahrenheitToCelcius(_model) {
+  const {temperature} = _model;
+  // T(°F) = T(°C) × 1.8 + 32
+  return ( (temperature * 1.8) + 32 );
+
+}
 
 export default initModel;
--- src/view.js
+++ src/view.js
@@ -2,20 +2,42 @@ import * as R from "ramda";
 import {h} from "virtual-dom";
 import hh from "hyperscript-helpers";
 
-const {pre, div, h1} = hh(h);
+const {pre, div, h1, input, select, option} = hh(h);
+
+const UNITS = ["Celcius", "Fahrenheit", "Kelvin"];
+
+function unitOptions(_selectedUnit) {
+  // transform selected unit into option tag
+  return R.map(item => option({value: item, selected: _selectedUnit === item}, item)
+  , UNITS);
+}
+
+function unitSection(_unit, _value) {
+    return div({className: "mw-50 ma1"}, [
+      input({
+        className: "pa2 w-100 mv2 input-reset db dim grow ba b--black-40"
+        , type: "text"
+        , value: _value
+      })
+      // , div({className: "pa2 w-40 mb2 dib tc black-40"}, "=")
+      , select(
+        {className: "db w-100 mv2 pa2 input-reset db dim ba b--black-40 bg-white"}
+        // unit select options
+        , unitOptions(_unit))
+    ]);
+}
 
 function view(_model) {
-  // return div({className: "mw6 center"}, [
-  //   h1({className: "f2 pv2 bb"}, "Temperature Unit Conversion Calculator")
-  //   , pre( JSON.stringify(_model, null, 2) )
-  // ]);
-
   return div({className: "mw6 center"}, [
     h1({className: "f2 pv2 bb"}, "Temperature Unit Conversion Calculator")
-    
+    // , cell(div, "pa2", 135)
+    , unitSection(UNITS[0], _model.leftValue)
     // creates pre-tag for pre-formated text
     , pre( JSON.stringify(_model, null, 2) )
   ]);
 }
+
+// helpers
+
 
 export default view;
